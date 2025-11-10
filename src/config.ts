3eb071f7d033a6cb6756d6a1a8cbfae130d64cdc
Dezsoe Babai <de.babai2000@gmail.com>
2025-11-10 11:39:29 +0100

    change site name
    Signed-off-by: Dezsoe Babai <de.babai2000@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -180,7 +180,7 @@ export interface SiteConfig {
 export const siteConfig: SiteConfig = {
   // Site Information
   // [CONFIG:SITE_URL]
-  site: "https://astro-modular.netlify.app",
+  site: "https://babaid.github.io",
   // [CONFIG:SITE_TITLE]
   title: "Astro Modular",
   // [CONFIG:SITE_DESCRIPTION]
